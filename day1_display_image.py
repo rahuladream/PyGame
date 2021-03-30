1fdc291ba903d829443441a1bb761f7f7083ee98
rahul singh <rahul.singh4800@gmail.com>
2021-03-30 23:46:24 +0530

    image display logo

--- day1_display_image.py
+++ day1_display_image.py
@@ -4,7 +4,8 @@ pygame.init()
 
 # Assign screen and size
 white = (255, 255, 255)
-size = (400, 400)
+size = (1000, 300)
+# Increased width as our image size is quite big in width
 
 # Defining the surface
 display_surface = pygame.display.set_mode(size)
@@ -13,4 +14,17 @@ display_surface = pygame.display.set_mode(size)
 pygame.display.set_caption('Image Display using PyGame')
 
 # Creating a surface object, image is drawn on it
-image = pygame.image.load(r'')
+image = pygame.image.load(r'assets/voltry_log.png')
+
+# Infinite Loop
+while True:
+    display_surface.fill(white)
+    display_surface.blit(image, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        # Draws the surface object to the screen
+        pygame.display.update()
+
